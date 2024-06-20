659744e361ef638ad88ff98c46c773ead4c3777e
Anish Gondhi <159081051+AnishGondhiDC@users.noreply.github.com>
2024-06-20 20:48:41 -0300

    easy-isanagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,6 +6,31 @@
 
 function isAnagram(str1, str2) {
 
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+
+  let temp1 = str1.split('');
+  let temp = str2.split('');
+  
+  if(temp1.length != temp.length){
+    return false;
+  }
+
+  let count  = temp.length;
+
+  for(let i =0 ; i<temp.length;i++){
+    for(let j=0; j< temp.length;j++){
+      if(temp[i] === temp1[j]){
+        count--;
+        temp1[j] = "";
+      }
+    }
+  }
+  if(count == 0){
+    return true;
+  }
+
+  return false;
 }
 
 module.exports = isAnagram;
