267016bd312fd8d5a50cf1cf866a30efd029fcb8
Anish Gondhi <159081051+AnishGondhiDC@users.noreply.github.com>
2024-06-26 16:34:48 -0300

    switch case

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -7,6 +7,22 @@
 
 function countVowels(str) {
     // Your code here
+
+    let vowelCount = 0;
+    for(let i =0 ;i<str.length; i++){
+      switch(str[i].toLowerCase()){
+        case 'a':
+        case 'e':
+        case 'i':  
+        case 'o':
+        case 'u':
+          vowelCount++;
+          break;
+        default:
+          continue;
+      }
+    }
+    return vowelCount;
 }
 
 module.exports = countVowels;
