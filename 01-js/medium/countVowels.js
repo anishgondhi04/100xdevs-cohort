8826f3f8672575113ae1e9485c48da51b951a025
Anish Gondhi <159081051+AnishGondhiDC@users.noreply.github.com>
2024-06-26 16:37:23 -0300

    formatted

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,23 +6,23 @@
 */
 
 function countVowels(str) {
-    // Your code here
+  // Your code here
 
-    let vowelCount = 0;
-    for(let i =0 ;i<str.length; i++){
-      switch(str[i].toLowerCase()){
-        case 'a':
-        case 'e':
-        case 'i':  
-        case 'o':
-        case 'u':
-          vowelCount++;
-          break;
-        default:
-          continue;
-      }
+  let vowelCount = 0;
+  for (let i = 0; i < str.length; i++) {
+    switch (str[i].toLowerCase()) {
+      case 'a':
+      case 'e':
+      case 'i':
+      case 'o':
+      case 'u':
+        vowelCount++;
+        break;
+      default:
+        continue;
     }
-    return vowelCount;
+  }
+  return vowelCount;
 }
 
 module.exports = countVowels;
