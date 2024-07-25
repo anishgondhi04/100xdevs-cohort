fa3ab8aa307510880c9019979535d739236d1e09
Anish Gondhi <159081051+AnishGondhiDC@users.noreply.github.com>
2024-07-25 20:42:00 -0300

    learning file reading

--- week-2/01-async-js/easy/practice.js
+++ week-2/01-async-js/easy/practice.js
@@ -1,10 +1,19 @@
-// Without using setInterval, try to code a counter in Javascript. There is a hint at the bottom of the file if you get stuck.
+// Write code to read contents of a file and print it to the console. 
+// You can use the fs library to as a black box, the goal is to understand async tasks. 
+// Try to do an expensive operation below the file read and see how it affects the output. 
+// Make the expensive operation more and more expensive and see how it affects the output. 
 
-let count = 0;
-const counter = () =>{
-    count++;
-    console.log(count);
-    setTimeout(counter, 1000);
-}
+const fs  = require('fs');
 
-counter();
+ fs.readFile('easy/test.txt', 'utf-8', (err,data)=>{
+    if(err){
+        console.error("Error Reading file", err);
+        return;
+    }
+    console.log(data);
+ });
+ let sum  = 0;
+ for(let i = 0; i < 100000000000; i+=2){
+    sum+=i;
+ }
+ console.log(sum);
