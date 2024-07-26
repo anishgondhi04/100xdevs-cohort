32eed70c04d397d3adca8ef6c4c15a796ac101a2
Anish Gondhi <159081051+AnishGondhiDC@users.noreply.github.com>
2024-07-25 21:09:19 -0300

    writeFile function

--- week-2/01-async-js/easy/practice.js
+++ week-2/01-async-js/easy/practice.js
@@ -1,19 +1,12 @@
-// Write code to read contents of a file and print it to the console. 
-// You can use the fs library to as a black box, the goal is to understand async tasks. 
-// Try to do an expensive operation below the file read and see how it affects the output. 
-// Make the expensive operation more and more expensive and see how it affects the output. 
+// ## Write to a file
+// Using the fs library again, try to write to the contents of a file.
+// You can use the fs library to as a black box, the goal is to understand async tasks.
 
-const fs  = require('fs');
+const fs = require("fs");
 
- fs.readFile('easy/test.txt', 'utf-8', (err,data)=>{
+fs.writeFile("easy/test.txt","\nHi this the new line //writing to the file",(err)=>{
     if(err){
-        console.error("Error Reading file", err);
-        return;
+        console.error("Error writing to the file",err);
     }
-    console.log(data);
- });
- let sum  = 0;
- for(let i = 0; i < 100000000000; i+=2){
-    sum+=i;
- }
- console.log(sum);
+    console.log("Write to file was successfull");
+})
